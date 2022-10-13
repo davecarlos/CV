848e5afbe9b99506a71a168b2467a98dc330ebd5
davecarlos <72527590+davecarlos@users.noreply.github.com>
2022-10-13 12:55:37 -0600

    Add files via upload

---
+++ common_variables.R
@@ -0,0 +1,21 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "My name is Carlos Dave and I am a graduate student at Boise State University.
+I grew up in Boise, ID and am passionate about innovation, research, and education in biological sciences.
+My current thesis investigates phenotypes and genomics of native sagebrush populations,
+utilizing modern sequencing and computing technologies at the forefront of bioinformatics.
+When free, I enjoy musical endeavors, international travel, and volleyball."
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "R",                  5,
+  "Javascript (d3.js)", 4.5,
+  "C++",                4,
+  "Python",             4,
+  "Bash",               3.5,
+  "SQL",                3,
+  "AWK",                3
+)
